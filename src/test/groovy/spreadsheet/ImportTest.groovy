a07e2e6219c5c989318a50d00549264a7f35a335
pernyf <per.nyfelt@nordnet.se>
2023-03-14 16:22:04 +0100

    add gradle plugins for publishing

--- src/test/groovy/spreadsheet/ImportTest.groovy
+++ src/test/groovy/spreadsheet/ImportTest.groovy
@@ -13,7 +13,7 @@ class ImportTest {
     @Test
     void testExcelImport() {
         def table = ExcelImporter.importExcelSheet(filePath: "Book1.xlsx", endRowNum: 11, endColNum: 4, firstRowAsColNames: true)
-        table = table.convert([id: Integer, bar: LocalDate, baz: BigDecimal], DateTimeFormatter.ofPattern('yyyy-MM-dd HH:mm:ss.SSS'))
+        table = table.convert(id: Integer, bar: LocalDate, baz: BigDecimal, DateTimeFormatter.ofPattern('yyyy-MM-dd HH:mm:ss.SSS'))
         //println(table.content())
         assertEquals(3, table[2, 0])
         assertEquals(LocalDate.parse("2023-05-06"), table[5, 2])
